80360a136e6d787ae6ff0942335ea69523e144e0
Amir Abdin <amab8901@protonmail.com>
2024-08-09 10:15:26 +0200

    first commit

---
+++ src/is_printable.rs
@@ -0,0 +1,48 @@
+pub trait IsPrintable {
+    fn is_printable(&self) -> bool;
+}
+
+impl IsPrintable for char {
+    // // self: char;
+    fn is_printable(&self) -> bool {
+        if self.is_control() {
+            return false;
+        }
+
+        if *self != ' ' && self.is_whitespace() {
+            return false;
+        }
+
+        let is_space_separator_other_than_space = is_space_separator_other_than_space(*self);
+
+        if is_space_separator_other_than_space {
+            return false;
+        }
+
+        true
+    }
+}
+
+fn is_space_separator_other_than_space(character: char) -> bool {
+    let is_space_separator_other_than_space = matches!(
+        character,
+        '\u{00A0}'
+            | '\u{1680}'
+            | '\u{2000}'
+            | '\u{2001}'
+            | '\u{2002}'
+            | '\u{2003}'
+            | '\u{2004}'
+            | '\u{2005}'
+            | '\u{2006}'
+            | '\u{2007}'
+            | '\u{2008}'
+            | '\u{2009}'
+            | '\u{200A}'
+            | '\u{202F}'
+            | '\u{205F}'
+            | '\u{3000}'
+    );
+
+    is_space_separator_other_than_space
+}
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+#![no_std]
+pub mod is_printable;
+pub use is_printable::IsPrintable;
