5f8353344199be1b46e524d43703a26acbc5ae73
Amir Abdin <amab8901@protonmail.com>
2024-08-11 08:39:50 +0200

    refac: simplify

--- src/is_printable.rs
+++ src/is_printable.rs
@@ -9,17 +9,26 @@ impl IsPrintable for char {
     fn is_printable(&self) -> bool {
         let escape_debug = self.escape_debug().to_string();
 
+        let is_typical_printable = is_typical_printable(&escape_debug);
         let is_special_printable = is_special_printable(&escape_debug);
-
-        let escapes = escape_debug.starts_with('\\');
-
-        let single_char = escape_debug.len() == 1;
-        let typical_printable = !escapes || single_char;
-
-        let is_printable = typical_printable || is_special_printable;
+        let is_printable = is_typical_printable || is_special_printable;
 
         is_printable
     }
+}
+
+fn is_typical_printable(escape_debug: &str) -> bool {
+    let escapes = escape_debug.starts_with('\\');
+    let typical_printable = !escapes;
+
+    typical_printable
+}
+
+fn is_special_printable(escape_debug: &str) -> bool {
+    let character = escape_debug.chars().last().unwrap();
+
+    let is_special_printable = matches!(character, '\'' | '\"' | '\\');
+    is_special_printable
 }
 
 impl IsPrintable for dyn Deref<Target = str> {
@@ -65,13 +74,6 @@ impl IsPrintable for &str {
 
         is_printable
     }
-}
-
-pub fn is_special_printable(escape_debug: &str) -> bool {
-    let character = escape_debug.chars().last().unwrap();
-
-    let is_special_printable = matches!(character, '\'' | '\"' | '\\' | '/');
-    is_special_printable
 }
 
 #[cfg(test)]
